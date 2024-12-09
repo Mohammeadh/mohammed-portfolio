aecd6b77fcf2c02e9b2a9a7ce16249aca7b3bdcb
Mohammed <130226741+Mohammeadh@users.noreply.github.com>
2024-12-09 09:49:11 +0530

    constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -11,7 +11,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/mohammeadh",
   resume:
-    "https://drive.google.com/file/d/1kqp5BaG5WNEyES5O6ClWm4XLlrbFbFO9/view?usp=sharing",
+    "https://drive.google.com/file/d/1dLT8Bs3wYAc5xOKfWL2pnWqwZgS3SNxB/view?usp=sharing",
   linkedin: "https://www.linkedin.com/mohammedh/",
   twitter: "https://twitter.com/RishavChanda",
   insta: "https://www.instagram.com/__mohammedh_",
