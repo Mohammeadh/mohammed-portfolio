d966f7b98f828d47595b922bb71ff29f4eff8106
Mohammed <130226741+Mohammeadh@users.noreply.github.com>
2024-12-13 12:38:24 +0530

    index.js

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -33,7 +33,7 @@ const HeroSection = () => {
 
                     <HeroRightContainer id="Right">
 
-                        <Img src={Hero} alt="hero-image" />
+                        <Img src={HeroImage} alt="hero-image" />
                     </HeroRightContainer>
                 </HeroInnerContainer>
 
