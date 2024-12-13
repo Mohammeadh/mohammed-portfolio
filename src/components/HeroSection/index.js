a2f72547f31d1146a1873093a14fa573112ab5c2
Mohammed <130226741+Mohammeadh@users.noreply.github.com>
2024-12-13 20:41:06 +0530

    Update index.js

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import HeroBgAnimation from '../HeroBgAnimation'
 import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle,SocialMediaIcons,SocialMediaIcon, ResumeButton } from './HeroStyle'
-import Hero from '../../images/Hero.jpg'
+import Hero from '../../images/Hero.JPG'
 import Typewriter from 'typewriter-effect';
 import { Bio } from '../../data/constants';
 
